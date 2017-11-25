8e1a86da07858ba7010c53ad3f742ba0d85cb2a3
xank <xank@qq.com>
2017-11-25 08:25:57 +0800

    init

---
+++ src/vue-birthday-input.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    :placeholder="placeholder"
+  >
+</template>
+
+<script>
+export default {
+  name: 'vue-birthday-input',
+  props: {
+    //placeholder.
+    placeholder: {
+      default: '出生日期',
+      required: false,
+      type: String,
+    }
+  },
+
+  methods: {
+  },
+
+  watch: {
+  },
+
+  mounted () {
+  }
+}
+</script>
