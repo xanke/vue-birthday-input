7fd364c3dba273f0757a1e859e01cbe56597eb17
xank <xank@qq.com>
2017-11-26 13:21:21 +0800

    全选BUG

--- src/vue-birthday-input.vue
+++ src/vue-birthday-input.vue
@@ -3,12 +3,10 @@
     id="vueBirthdayInput"
     ref="vueBirthdayInput"
     :placeholder="placeholder"
-
     @focus="onFocusHandler"
     @click="onFocusHandler"
     @mouseover="onMouseoverHandel"
     @mouseout="onMouseoutHandel"
-
     v-model="birthday"
   >
 </template>
@@ -46,17 +44,14 @@ export default {
         this.birthday = inputDefault
         pos = 0
       }
-      
       if (fpos > pos) {
         this.setCaretPosition(pos)
       }
- 
     },
     //鼠标经过
     onMouseoverHandel (e) {
       if (this.birthday == '') {
         this.birthday = inputDefault
-        this.pos = 0
       }
     },
     //鼠标退出
@@ -70,13 +65,13 @@ export default {
       let ctrl = document.getElementById('vueBirthdayInput')
       var CaretPos = 0   // IE Support
       if (document.selection) {
-          var Sel = document.selection.createRange()
-          Sel.moveStart ('character', - ctrl.value.length)
-          CaretPos = Sel.text.length
+        var Sel = document.selection.createRange()
+        Sel.moveStart ('character', - ctrl.value.length)
+        CaretPos = Sel.text.length
       }
       // Firefox support
       else if (ctrl.selectionStart || ctrl.selectionStart == '0')
-          CaretPos = ctrl.selectionStart
+        CaretPos = ctrl.selectionStart
       return (CaretPos)
     },
     // 设置光标位置
@@ -96,24 +91,46 @@ export default {
         range.moveStart('character', pos)
         range.select()
       }
-      
     },
     format(nVal) {
       let pos = this.pos
       let bob = nVal.substring(0, pos + 1) + nVal.substring(pos + 2, nVal.length )
-      return bob
+
+      //与默认对比
+      let diff = ''
+      for (let i = 0; i < inputDefault.length; i ++) {
+        if (bob[i] != inputDefault[i]) {
+          diff = diff + bob[i]
+        }
+      }
+      //输入非数字跳回
+      let patrn =  /^[0-9]*$/;  
+      if (!patrn.test(diff)) {
+        return false
+      } else {
+        return bob
+      }
     }
   },
 
   watch: {
-    birthday (nVal) {
+    birthday (nVal, oVal) {
       this.$emit('change', nVal)
       if (nVal === inputDefault || !nVal) {
         return
       }
-
+      //如被全选输入
+      if (nVal.length == 1) {
+        this.birthday = inputDefault
+      }
+      //正常输入
       if (nVal.length > 11) {
-        this.birthday = this.format(nVal)
+        let val = this.format(nVal)
+        if (val) {
+          this.birthday = val
+        } else {
+          this.birthday = oVal
+        }
         return
       }
 
